2cc87bf1ddf4e22cef6d182feff026e6040f8015
Shabnam09dev <muhayyoabrorova6@gmail.com>
2025-11-18 05:00:14 -0800

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,17 @@
+dustim = {
+ "ismi" : "Javohir",
+ 'yoshi' : "11",
+ 'mahalla' : "Beruniy",
+ "tugilgan kun" : "26Mart"
+}
+print(f"Do'stim ismi : {dustim['ismi']} . U {dustim['yoshi']} yoshda . Uning tugilgan kuni {dustim['tugilgan kun']}ta")
+
+python_izohli_lugati = {
+    'integer':"Butun son",
+    'float':"O'nlik son",
+    'string':"Matn",
+    'list':"Ro'yxat" 
+    }
+print(f"integer so'zining manosi - {python_izohli_lugati['integer']}")
+
+kalit = input("Kalit so'z kiriting:")
